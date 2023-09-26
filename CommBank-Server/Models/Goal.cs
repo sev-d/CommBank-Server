70300ffc27e565961c78db2784f0a926aa82e6d0
severine <127178206+sev-d@users.noreply.github.com>
2023-09-26 16:37:45 +0930

    Added an optional public Icon field of string type

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -1,30 +1,36 @@
-﻿using MongoDB.Bson;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
 
-namespace CommBank.Models;
-
-public class Goal
+namespace CommBank.Models
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public class Goal
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
-    public string? Name { get; set; }
+        public string? Name { get; set; }
 
-    public UInt64 TargetAmount { get; set; } = 0;
+        public UInt64 TargetAmount { get; set; } = 0;
 
-    public DateTime TargetDate { get; set; }
+        public DateTime TargetDate { get; set; }
 
-    public double Balance { get; set; } = 0.00;
+        public double Balance { get; set; } = 0.00;
 
-    public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; } = DateTime.Now;
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public List<string>? TransactionIds { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string>? TransactionIds { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public List<string>? TagIds { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string>? TagIds { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? UserId { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? UserId { get; set; }
+
+        // Add the Icon property here
+        public string? Icon { get; set; }
+    }
 }
